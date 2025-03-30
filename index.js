7db9fc1867994af7dfb715b81c1807a1b60fc278
biwott-v <victor.biwott@student.moringaschool.com>
2025-03-31 02:21:31 +0300

    completes assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+class Polygon {
+    constructor(sides) {
+      this.sides = sides;
+    }
+  
+    get countSides() {
+      return this.sides.length;
+    }
+  
+    get perimeter() {
+      return this.sides.reduce((sum, side) => sum + side, 0);
+    }
+  }
+  
+  class Triangle extends Polygon {
+    get isValid() {
+      if (this.countSides !== 3) return false;
+      const [a, b, c] = this.sides.sort((x, y) => x - y);
+      return a + b > c;
+    }
+  }
+  
+  class Square extends Polygon {
+    get isValid() {
+      return this.countSides === 4 && this.sides.every(side => side === this.sides[0]);
+    }
+  
+    get area() {
+      return this.sides[0] ** 2;
+    }
+  }
